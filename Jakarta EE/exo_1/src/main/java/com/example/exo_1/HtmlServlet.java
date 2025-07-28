08aa2279ed584dcad588249317d6a26b7c15ac42
Rémi Debruyne <remi.debruyne@stereograph.fr>
2025-07-28 14:04:10 +0200

    add exo_1 Jakarta

---
+++ Jakarta EE/exo_1/src/main/java/com/example/exo_1/HtmlServlet.java
@@ -0,0 +1,21 @@
+package com.example.exo_1;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "htmlServlet", value ="/html")
+public class HtmlServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "Html" + "</h1>");
+        out.println("</body></html>");
+    }
+}
