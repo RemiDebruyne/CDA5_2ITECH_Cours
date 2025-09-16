9c6d0d0d93c7241cd70346339677179a1b57822a
remiD <remidebruyne@hotmail.com>
2025-09-16 14:31:18 +0200

    exo(react): move exo_gallery in its on project

--- React/exo_gallery/src/main.jsx
+++ React/exo_gallery/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { Quizz } from './components/Quizz/Quizz';
-
+import { Gallery } from './components/Gallery/Gallery';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Quizz />
+    <Gallery />
   </StrictMode>
 );
