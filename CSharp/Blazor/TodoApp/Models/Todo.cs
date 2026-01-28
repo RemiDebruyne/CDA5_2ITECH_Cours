7cf877c1561d70440bbc91d71bca6b3cc0df10ae
remiD <remidebruyne@hotmail.com>
2026-01-28 14:13:59 +0100

    exo(blazor): exo todo

---
+++ CSharp/Blazor/TodoApp/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApp.Models;
+
+public class Todo
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Task { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public DateTime CreationDate { get; set; } = DateTime.Now;
+
+}
