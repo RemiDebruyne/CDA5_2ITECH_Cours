99194a0f7142dd84d7097a5e029aa54aa230a15f
remiD <remidebruyne@hotmail.com>
2026-02-20 16:52:58 +0100

    exo:(ci-cd): add ci-cd exercices, check .gitlab-ci.yml for exercices.

---
+++ CI-CD/exo-react-app/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+    exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**'],
+    reporters: ['verbose', 'junit', 'html'],
+    outputFile: {
+      junit: './reports/junit.xml',
+      html: './reports/html/index.html',
+    },
+  },
+});
